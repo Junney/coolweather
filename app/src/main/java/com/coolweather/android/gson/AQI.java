5af8683427f22dd7f5a224e3ac60316e9af44745
iChuan <yichuan0424@126.com>
2017-08-02 18:15:43 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.coolweather.android.gson;
+
+/**
+ * @author apple
+ * @version 1.0
+ */
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+}
